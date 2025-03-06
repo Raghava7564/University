5b52cdb2eee0f639af11c871a6f77a24802b8f0a
Raghava7564 <raghavalakkamraju@gmail.com>
2025-03-06 16:41:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,53 @@
-# Azure Container Registry
+# Configure the Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup"
+  location = "East US"
+}
+
+# Create Azure Container Registry (ACR)
 resource "azurerm_container_registry" "acr" {
-  name                = var.acr_name
+  name                = "myacrregistry"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  sku                 = "Basic"
+  sku                 = "Basic" # Options: Basic, Standard, Premium
   admin_enabled       = true
 }
 
-resource "azurerm_linux_web_app" "webapp" {
-  name                = var.web_app_name
-  location            = azurerm_resource_group.rg.location
+# Create App Service Plan
+resource "azurerm_service_plan" "asp" {
+  name                = "myAppServicePlan"
   resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "B1" # Use F1 for Free, B1 for Basic
+}
+
+# Create Web App
+resource "azurerm_linux_web_app" "webapp" {
+  name                = "mywebapp"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   service_plan_id     = azurerm_service_plan.asp.id
 
   site_config {
     always_on = true
-    linux_fx_version = "DOCKER|${var.acr_name}.azurecr.io/mywebapp:${var.image_tag}"
+    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.login_server}/mywebapp:latest"
   }
 
-  identity {
-    type = "SystemAssigned"
+  app_settings = {
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
+    DOCKER_REGISTRY_SERVER_URL          = "https://${azurerm_container_registry.acr.login_server}"
+    DOCKER_REGISTRY_SERVER_USERNAME     = azurerm_container_registry.acr.admin_username
+    DOCKER_REGISTRY_SERVER_PASSWORD     = azurerm_container_registry.acr.admin_password
   }
+}
+
+# Output ACR Login Server
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
 }
