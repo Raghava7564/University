6c9d7ea8a21282111df0b9c1a46edb99eb7fb901
Raghava7564 <raghavalakkamraju@gmail.com>
2025-03-06 16:44:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 # Configure the Azure Provider
 provider "azurerm" {
   features {}
+
+  subscription_id = "d9f05757-f3b3-4460-aa6c-da6ed3898008"
 }
+
 
 # Create Resource Group
 resource "azurerm_resource_group" "rg" {
