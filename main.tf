6f632dd85acf5f0b1b6a54d57f6b0f008437a8c3
Raghava7564 <raghavalakkamraju@gmail.com>
2025-03-06 16:35:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+# Azure Container Registry
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
 resource "azurerm_linux_web_app" "webapp" {
   name                = var.web_app_name
   location            = azurerm_resource_group.rg.location
