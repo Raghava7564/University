6757fd6bf7da39825efa3c7fac501c99c1e151ed
Raghava7564 <raghavalakkamraju@gmail.com>
2025-03-06 16:09:02 +0530

    Update variables.tf

--- college code web page/variables.tf
+++ college code web page/variables.tf
@@ -1,5 +1,28 @@
-variable "service_plan_name" {
-  description = "The name of the Azure App Service Plan"
-  type        = string
-  default     = "myAppServicePlan"
+# Variables
+variable "resource_group_name" {
+  default = "myResourceGroup"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "acr_name" {
+  default = "myacr"
+}
+
+variable "acr_sku" {
+  default = "Basic"
+}
+
+variable "app_service_plan_name" {
+  default = "myAppServicePlan"
+}
+
+variable "web_app_name" {
+  default = "myWebApp"
+}
+
+variable "app_service_sku" {
+  default = "P1v2"  # Premium for better performance
 }
