4ddbc11c1ec58bd2c8dab87a48510eed198d8c8d
Raghava7564 <raghavalakkamraju@gmail.com>
2025-03-06 16:08:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,36 +2,6 @@
 provider "azurerm" {
   features {}
 }
-
-# Variables
-variable "resource_group_name" {
-  default = "myResourceGroup"
-}
-
-variable "location" {
-  default = "East US"
-}
-
-variable "acr_name" {
-  default = "myacr"
-}
-
-variable "acr_sku" {
-  default = "Basic"
-}
-
-variable "app_service_plan_name" {
-  default = "myAppServicePlan"
-}
-
-variable "web_app_name" {
-  default = "myWebApp"
-}
-
-variable "app_service_sku" {
-  default = "P1v2"  # Premium for better performance
-}
-
 # Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
