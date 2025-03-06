dc526f568531516647f1f904c883e926ccf2b32b
Raghava7564 <raghavalakkamraju@gmail.com>
2025-03-06 16:12:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,49 +1,15 @@
-# Configure the Azure Provider
-provider "azurerm" {
-  features {}
-}
-# Resource Group
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-# Azure Container Registry (ACR)
-resource "azurerm_container_registry" "acr" {
-  name                = var.acr_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = var.acr_sku
-  admin_enabled       = true
-}
-
-# App Service Plan
-resource "azurerm_service_plan" "asp" {
-  name                = var.app_service_plan_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Windows"
-  sku_name            = var.app_service_sku
-}
-
-# Web App (Windows) pulling image from ACR
-resource "azurerm_windows_web_app" "webapp" {
+resource "azurerm_linux_web_app" "webapp" {
   name                = var.web_app_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.asp.id
 
   site_config {
-    always_on           = true
-    app_command_line    = ""
-    ftps_state          = "Disabled"
-    acr_use_managed_identity = false
-    windows_fx_version  = "DOCKER|${azurerm_container_registry.acr.login_server}/mywebapp:latest"
+    always_on = true
+    linux_fx_version = "DOCKER|${var.acr_name}.azurecr.io/mywebapp:${var.image_tag}"
   }
 
-  app_settings = {
-    "DOCKER_REGISTRY_SERVER_URL"      = "https://${azurerm_container_registry.acr.login_server}"
-    "DOCKER_REGISTRY_SERVER_USERNAME" = azurerm_container_registry.acr.admin_username
-    "DOCKER_REGISTRY_SERVER_PASSWORD" = azurerm_container_registry.acr.admin_password
+  identity {
+    type = "SystemAssigned"
   }
 }
