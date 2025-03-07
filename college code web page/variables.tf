5ba5e1ffff4ec9a72ec12b1aec6727345ecf6045
Raghava7564 <raghavalakkamraju@gmail.com>
2025-03-07 11:17:46 +0530

    Update variables.tf

--- college code web page/variables.tf
+++ college code web page/variables.tf
@@ -1,36 +1,48 @@
 # Variables
 variable "resource_group_name" {
-  default = "myResourceGroup"
+  description = "Name of the Resource Group"
+  type        = string
+  default     = "myResourceGroup"
 }
 
 variable "location" {
-  default = "East US"
+  description = "Azure region for resource deployment"
+  type        = string
+  default     = "East US"
 }
 
 variable "acr_name" {
   description = "The name of the Azure Container Registry"
   type        = string
-  default     = "myuniqueregistry"  # Change to a unique name
+  default     = "myuniqueregistry"
 }
 
 variable "acr_sku" {
-  default = "Basic"
+  description = "SKU tier for the Azure Container Registry"
+  type        = string
+  default     = "Basic"
 }
 
 variable "app_service_plan_name" {
-  default = "myAppServicePlan"
+  description = "Name of the App Service Plan"
+  type        = string
+  default     = "myAppServicePlan"
 }
 
 variable "web_app_name" {
-  default = "myWebApp"
+  description = "Name of the Web App"
+  type        = string
+  default     = "myWebApp"
 }
 
 variable "app_service_sku" {
-  default = "P1v2"  # Premium for better performance
+  description = "SKU tier for the App Service Plan"
+  type        = string
+  default     = "P1v2"
 }
 
 variable "image_tag" {
   description = "Tag of the Docker image"
   type        = string
-  default     = "latest"  # Optional: Set a default value
+  default     = "latest"
 }
