724916b9a2a22bd604a3ab201bdddfb1143bf50a
Raghava7564 <raghavalakkamraju@gmail.com>
2025-03-07 14:14:51 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -45,7 +45,7 @@ resource "azurerm_linux_web_app" "webapp" {
   site_config {
     application_stack {
       docker_image_name = "${azurerm_container_registry.acr.login_server}/mywebapp"
-      docker_image_tag  = var.image_tag
+      docker_image_name  = var.image_tag
     }
   }
 
