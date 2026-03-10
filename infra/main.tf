11bda3420501b019f2ded66eeed515a1d2ee0e11
Raghava7564 <raghavalakkamraju@gmail.com>
2026-03-10 15:30:30 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,3 @@
-# Configure the Azure Provider
-provider "azurerm" {
-  features {}
-  subscription_id = "d9f05757-f3b3-4460-aa6c-da6ed3898008"
-}
 # Create Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name # ✅ Using variable
